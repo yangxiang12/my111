2d578810278c1e12122238db94f90dcb5a68dd2e
yangxiang <18614085772@163.com>
2017-09-01 09:36:59 +0800

    111

--- test.py
+++ test.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+li = [
+    {'a':1,'b':2},
+    {'a':3,'b':2},
+    {'a':2,'b':2},
+    {'a':8,'b':2},
+    {'a':5,'b':2},
+]
+
+
+r1 =[1,2,3,7]
+r2 = [2,3,5,6]
+
+
+# print(set(r1)-set(r2))
+
+
+
+
+# a = [3,-2,1,-4]
+# l = [abs(i) for i in a]
+# print(sorted(l))
+# print()
+
+
