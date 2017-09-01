128ddc4ecd8f3a83a8ab079a175453e7fe3e796b
yangxiang <18614085772@163.com>
2017-09-01 19:44:43 +0800

    chouti

--- my111/spiders/chouti.py
+++ my111/spiders/chouti.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.selector import HtmlXPathSelector
-from scrapy.http.request import Request
-from scrapy.http.cookies import CookieJar
-from scrapy import FormRequest
+from scrapy.http import Request
+from scrapy.selector import Selector
 
 
 class ChouTiSpider(scrapy.Spider):
@@ -11,74 +9,66 @@ class ChouTiSpider(scrapy.Spider):
     name = "chouti"
     # 允许的域名
     allowed_domains = ["chouti.com"]
+    start_url = ['http://chouti.com/']
+    cookies_dict = {}
+    """
+    1.发送一个GET请求，抽屉
+        获取cookies
+    2.用户密码POST登录：携带上一次cookie
+        返回值：9999
 
-    cookie_dict = {}
-    has_request_set = {}
+    3.为所欲为：携带cookie
+    """
 
     def start_requests(self):
-        url = 'http://dig.chouti.com/'
-        # return [Request(url=url, callback=self.login)]
-        yield Request(url=url, callback=self.login)
+        for url in self.start_url:
+            #dont_filter:过滤，
+            yield Request(url,dont_filter=True,callback=self.parse1)
 
-    def login(self, response):
-        cookie_jar = CookieJar()
-        cookie_jar.extract_cookies(response, response.request)
-        for k, v in cookie_jar._cookies.items():
-            for i, j in v.items():
-                for m, n in j.items():
-                    self.cookie_dict[m] = n.value
+    def parse1(self,response):
+        #response.text首页所有内容
+        from scrapy.http.cookies import CookieJar
+        cookie_jar = CookieJar()#对象中封装了cookies
+        cookie_jar.extract_cookies(response,response.request)#去响应中获取cookies
 
-        req = Request(
-            url='http://dig.chouti.com/login',
-            method='POST',
-            headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-            body='phone=8618614085772&password=yang243667673&oneMonth=1',
-            cookies=self.cookie_dict,
-            callback=self.check_login
+        for k,v in cookie_jar._cookies.items():
+            for i,j in v.items():
+                for m,n in j.items():
+                    self.cookies_dict[m] = n.value
+
+        post_dict = {
+            'phone':'8618614085772',
+            'password':'yang243667673',
+            'oneMonth':1,
+        }
+        import urllib.parse
+
+        #目的：发送POST进行登录
+        yield  Request(
+            url="http://dig.chouti.com/login",
+            method="POST",
+            cookies=self.cookies_dict,
+            body=urllib.parse.urlencode(post_dict),#将字典转换成phone=8618614085772&password=yang243667673
+            headers={'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'},
+            callback=self.parse2
         )
-        yield req
 
-    def check_login(self, response):
-        req = Request(
-            url='http://dig.chouti.com/',
-            method='GET',
-            callback=self.show,
-            cookies=self.cookie_dict,
-            dont_filter=True
-        )
-        yield req
+    def parse2(self, response):
+        print(response.text)
+        print('......')
+        #获取新闻列表
+        yield Request(url='http://dig.chouti.com/',cookies=self.cookies_dict,callback=self.parse3)
 
-    def show(self, response):
-        # print(response)
-        hxs = HtmlXPathSelector(response)
-        news_list = hxs.select('//div[@id="content-list"]/div[@class="item"]')
-        for new in news_list:
-            # temp = new.xpath('div/div[@class="part2"]/@share-linkid').extract()
-            link_id = new.xpath('*/div[@class="part2"]/@share-linkid').extract_first()
-            yield Request(
-                url='http://dig.chouti.com/link/vote?linksId=%s' %(link_id,),
-                method='POST',
-                cookies=self.cookie_dict,
-                callback=self.do_favor
-            )
+    def parse3(self, response):
+        #找到div,class=part2,获取share-Linkid
+        print('parse3...')
+        print(response.text)
+        hxs = Selector(response)
+        link_id_list = hxs.xpath('//div[@class="part2"]/@share-Linkid').extract()
+        print(link_id_list)
+        for link_id in link_id_list:
+            base_url = "http://dig.chouti.com/link/vote?linksId=%s"%link_id
+            yield Request(url=base_url,method='POST',cookies=self.cookies_dict,callback=self.parse4)
 
-        page_list = hxs.select('//div[@id="dig_lcpage"]//a[re:test(@href, "/all/hot/recent/\d+")]/@href').extract()
-        for page in page_list:
-
-            page_url = 'http://dig.chouti.com%s' % page
-            import hashlib
-            hash = hashlib.md5()
-            hash.update(bytes(page_url,encoding='utf-8'))
-            key = hash.hexdigest()
-            if key in self.has_request_set:
-                pass
-            else:
-                self.has_request_set[key] = page_url
-                yield Request(
-                    url=page_url,
-                    method='GET',
-                    callback=self.show
-                )
-
-    def do_favor(self, response):
+    def parse4(self, response):
         print(response.text)
